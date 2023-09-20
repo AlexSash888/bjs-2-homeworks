441876d5c46c6615d3ab0d9a0845b9c2f6a63551
Alexander <107877801+AlexSash888@users.noreply.github.com>
2023-09-20 05:45:31 +0300

    Update task.js commit10

--- 4.objects/task.js
+++ 4.objects/task.js
@@ -1,8 +1,8 @@
 function Student(name, gender, age) {
-    this.name = name,
-    this.gender = gender,
-    this.age = age,
-    this.marks = [];
+    (this.name = name),
+    (this.gender = gender),
+    (this.age = age),
+    (this.marks = []);
 }
 
 let student1 = new Student("Alex", "male", 31);
@@ -11,27 +11,27 @@ let student3 = new Student("Mike", "male", 29);
 let student4 = new Student("Boris", "male", 69);
 
 Student.prototype.setSubject = function (subjectName) {
-  this.subject = subjectName;
+ this.subject = subjectName;
 };
 let student = new Student("Caleb", "male", 24);
 student.setSubject("A");
 
 Student.prototype.addMarks = function (...marks) {
   if (this.marks !== undefined) {
-     this.marks.push(...marks);
+    this.marks.push(...marks);
   }
 };
 
 Student.prototype.getAverage = function () {
   let sum = 0;
-  let l = 0;
-  if (this.marks !== undefined) {
-    l = this.marks.length;
+  let list = 0;
+  if (this.marks !== undefined) && (this.marks === 0){
+    return 0;
   }
-
   for (let i = 0; i < l; i++) {
     sum = sum + Number(this.marks[i]);
   }
+  return sum / l;
 };
 
 Student.prototype.exclude = function (reason) {
